ab8a266322748d8ba5ee0a655d581313a6cdd6c7
ciron <chiron.cse1998@gmail.com>
2020-10-31 05:06:24 +0600

    all is done of project blog now just details api is due now

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Category;
+use Brian2694\Toastr\Facades\Toastr;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categories = Category::latest()->get();
+        return view('admin.category.index',compact('categories'));
+    }
+
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.category.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'name' => 'required|unique:categories',          
+        ]);
+        $slug = str_slug($request->name);
+
+        $category = new Category();
+        $category->name = $request->name;
+        $category->slug = $slug;
+      
+        $category->save();
+        Toastr::success('Department Successfully Saved :)' ,'Success');
+        return redirect()->route('admin.category.index');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $category = Category::find($id);
+        return view('admin.category.edit',compact('category'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request,[
+            'name' => 'required',          
+        ]);
+        // get form image
+        $image = $request->file('image');
+        $slug = str_slug($request->name);
+        $category = Category::find($id);
+//        
+
+        $category->name = $request->name;
+        $category->slug = $slug;
+        // $category->image = $imagename;
+        $category->save();
+        Toastr::success('Department Successfully Updated :)' ,'Success');
+        return redirect()->route('admin.category.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $category = Category::find($id);
+      
+        $category->delete();
+        Toastr::success('Department Successfully Deleted :)','Success');
+        return redirect()->back();
+    }
+}
